7f4033d789fd657415d053bb4c120239ea21260c
brendanhay <brendan.g.hay@gmail.com>
2012-04-21 22:24:50 +0200

    Removing case in favor of fromJust

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,15 @@
 module Main where
 
-import Control.Monad.Trans
+import Data.Maybe (fromJust)
 import System.Environment (getEnv)
 import Network.URI (parseURI)
 import Monitor.RabbitMQ (overview)
 
 env = "RABBITMQ_MGMT_URI"
 
-uri u = case parseURI u of
-    Just u  -> u
-    Nothing -> error $ "Syntax error in " ++ env
-
 main :: IO ()
 main = do
     res <- getEnv env
     -- res <- overview
-    print $ uri res
+    print $ fromJust $ parseURI res
 
