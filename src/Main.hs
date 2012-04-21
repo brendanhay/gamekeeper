41ce5c738b64ebebffd7c484d7ae5e2304bcf65f
brendanhay <brendan.g.hay@gmail.com>
2012-04-21 22:18:34 +0200

    Adding uri parsing for the rabbit mgmt env variable

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
+import Control.Monad.Trans
 import System.Environment (getEnv)
+import Network.URI (parseURI)
 import Monitor.RabbitMQ (overview)
+
+env = "RABBITMQ_MGMT_URI"
+
+uri u = case parseURI u of
+    Just u  -> u
+    Nothing -> error $ "Syntax error in " ++ env
 
 main :: IO ()
 main = do
-    res <- getEnv "RABBITMQ_MGMT_URI"
+    res <- getEnv env
     -- res <- overview
-    print res
+    print $ uri res
 
