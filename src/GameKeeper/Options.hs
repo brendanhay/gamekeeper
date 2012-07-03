14b1773553619bde8f69b5c42bf970950bfab5f8
brendanhay <brendan.g.hay@gmail.com>
2012-07-03 17:15:57 +0200

    Adding licensing headers

--- src/GameKeeper/Options.hs
+++ src/GameKeeper/Options.hs
@@ -1,5 +1,17 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable, RecordWildCards #-}
 
+-- |
+-- Module      : GameKeeper.Options
+-- Copyright   : (c) 2012 Brendan Hay <brendan@soundcloud.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan@soundcloud.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
 module GameKeeper.Options
     ( Options(..)
     , parseOptions
