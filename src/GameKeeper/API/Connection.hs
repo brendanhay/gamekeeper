d9dec3e2bc8c867fd45a55a325eccdc16369e6ed
brendanhay <brendan.g.hay@gmail.com>
2012-07-20 14:30:02 +0200

    Re-adding due to case-insensitive-file-system-shit

---
+++ src/GameKeeper/API/Connection.hs
@@ -0,0 +1,77 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-- |
+-- Module      : GameKeeper.Api.Connection
+-- Copyright   : (c) 2012 Brendan Hay <brendan@soundcloud.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan@soundcloud.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module GameKeeper.API.Connection (
+    Connection
+  , list
+  , idle
+  ) where
+
+import Prelude hiding        (product)
+import Control.Applicative   ((<$>), (<*>), empty)
+import Control.Monad         (liftM)
+import Data.Aeson            (decode')
+import Data.Aeson.Types
+import Data.Data
+import Data.Time.Clock.POSIX (POSIXTime, getPOSIXTime)
+import Data.Vector           (Vector, toList)
+import GameKeeper.Http
+
+data Connection = Connection
+    { name      :: String
+    , user      :: String
+    , last_recv :: POSIXTime
+    , last_send :: POSIXTime
+    } deriving (Show, Data, Typeable)
+
+instance FromJSON Connection where
+    parseJSON (Object o) = Connection
+        <$> o .: "name"
+        <*> o .: "user"
+        <*> ((o .: "recv_oct_details") >>= (.: "last_event"))
+        <*> ((o .: "send_oct_details") >>= (.: "last_event"))
+    parseJSON _ = empty
+
+instance FromJSON POSIXTime where
+    parseJSON json = liftM msToSeconds (parseJSON json)
+
+--
+-- API
+--
+
+list :: String -> IO [Connection]
+list uri = do
+    body <- getBody $ concat [uri, "api/connections", qs]
+    return $ case (decode' body :: Maybe (Vector Connection)) of
+        Just v  -> toList v
+        Nothing -> []
+  where
+    qs = "?columns=name,user,recv_oct_details.last_event,send_oct_details.last_event"
+
+idle :: String -> Integer -> IO [Connection]
+idle uri days = do
+    time <- getPOSIXTime
+    list uri >>= return . filter (stale days time)
+
+--
+-- Private
+--
+
+stale :: Integer -> POSIXTime -> Connection -> Bool
+stale days time Connection{..} = and $ map diff [last_recv, last_send]
+  where
+    diff n = time >= n + 86400 * fromIntegral days
+
+msToSeconds :: Integer -> POSIXTime
+msToSeconds = realToFrac . (/ (1000 :: Double)) . fromIntegral
