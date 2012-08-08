f8e24ad84756c36be2c9025bcf931bb630b3c3db
brendanhay <brendan.g.hay@gmail.com>
2012-08-08 15:37:37 +0200

    Adjusting nagios types in an attempt to make a more usable API

--- src/GameKeeper/Nagios.hs
+++ src/GameKeeper/Nagios.hs
@@ -15,71 +15,81 @@
 module GameKeeper.Nagios (
     -- * Exported Types
       Health(..)
-    , Service(..)
+    , Service
+    , Message
     , Status(..)
     , Check(..)
 
     -- * Functions
-    , exitWith
+    , check
     ) where
 
 import Data.Char (toUpper)
-import Data.Data (Data, Typeable)
-import Data.Word (Word16)
-import Network.Socket
-import GameKeeper.Logger
 
 import qualified Data.ByteString.Char8 as BS
 import qualified System.Exit           as E
 
-data Health = Health
-    { healthWarn :: Double
-    , healthCrit :: Double
-    } deriving (Eq, Show)
+data Health  = Health Double Double deriving (Eq, Show)
 
-newtype Service = Service BS.ByteString deriving (Show)
+type Service = BS.ByteString
 
-data Status
-    = OK       -- ^ The plugin was able to check the service and
-               --   it appeared to be functioning properly
-    | Warning  -- ^ The plugin was able to check the service,
-               --   but it appeared to be above some "warning"
-               --   threshold or did not appear to be working properly
-    | Critical -- ^ The plugin detected that either the service was
-               --   not running or it was above some "critical" threshold
-    | Unknown  -- ^ Invalid command line arguments were supplied
-               --   to the plugin or low-level failures internal
-               --   to the plugin (such as unable to fork, or open a tcp socket)
-               --   that prevent it from performing the specified operation.
-               --   Higher-level errors (such as name resolution errors, socket timeouts, etc)
-               --   are outside of the control of plugins and should
-               --   generally NOT be reported as UNKNOWN states.
-      deriving (Eq, Enum, Show)
+type Message = Double -> BS.ByteString
 
 data Check = Check
-    { service :: Service
-    , status  :: Status
-    , text    :: BS.ByteString
-    } deriving (Show)
+    { service  :: Service
+    , health   :: Health
+    , ok       :: Message
+    , warning  :: Message
+    , critical :: Message
+    , unknown  :: BS.ByteString
+    }
+
+data Status
+    = OK
+      -- ^ The plugin was able to check the service and
+      --   it appeared to be functioning properly
+    | Warning
+      -- ^ The plugin was able to check the service,
+      --   but it appeared to be above some "warning"
+      --   threshold or did not appear to be working properly
+    | Critical
+      -- ^ The plugin detected that either the service was
+      --   not running or it was above some "critical" threshold
+    | Unknown
+      -- ^ Invalid command line arguments were supplied
+      --   to the plugin or low-level failures internal
+      --   to the plugin (such as unable to fork, or open a tcp socket)
+      --   that prevent it from performing the specified operation.
+      --   Higher-level errors (such as name resolution errors, socket timeouts, etc)
+      --   are outside of the control of plugins and should
+      --   generally NOT be reported as UNKNOWN states.
+      deriving (Eq, Enum, Show)
 
 --
 -- API
 --
 
-exitWith :: Check -> IO ()
-exitWith chk = BS.putStrLn (toByteString chk) >> E.exitWith (toExitCode chk)
+check :: Maybe Double -> Check -> IO ()
+check n chk@Check{..} = do
+    BS.putStrLn output
+    E.exitWith $ code s
+  where
+    (s, m) = status n chk
+    pack   = BS.pack . map toUpper . show
+    output = BS.concat [service, " ", pack s, " - ", m]
 
 --
 -- Private
 --
 
-toExitCode :: Check -> E.ExitCode
-toExitCode Check{..} | status == OK = E.ExitSuccess
-                     | otherwise    = E.ExitFailure (fromEnum status)
-
-toByteString :: Check -> BS.ByteString
-toByteString Check{..} = BS.concat [bserv service, " ", bstat status, " - ", text]
+status :: Maybe Double -> Check -> (Status, BS.ByteString)
+status Nothing  Check{..}             = (Unknown, unknown)
+status (Just n) Check{..} | n >= x    = (Warning, warning n)
+                          | n >= y    = (Critical, critical n)
+                          | otherwise = (OK, ok n)
   where
-    bserv (Service s) = s
-    bstat             = BS.pack . map toUpper . show
+    (Health x y) = health
 
+code :: Status -> E.ExitCode
+code OK = E.ExitSuccess
+code s  = E.ExitFailure (fromEnum s)
