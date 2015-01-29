052804af052ead93032c47fe8e43cf9b8829c657
Brendan Hay <brendan.g.hay@gmail.com>
2015-01-29 11:32:28 +0100

    Updating licensing dates

--- src/GameKeeper/API/Overview.hs
+++ src/GameKeeper/API/Overview.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      : GameKeeper.API.Overview
--- Copyright   : (c) 2012 Brendan Hay <brendan@soundcloud.com>
+-- Copyright   : (c) 2012-2015 Brendan Hay <brendan@soundcloud.com>
 -- License     : This Source Code Form is subject to the terms of
 --               the Mozilla Public License, v. 2.0.
 --               A copy of the MPL can be found in the LICENSE file or
--- src/GameKeeper/Options.hs
+++ src/GameKeeper/Options.hs
@@ -2,7 +2,7 @@
 
 -- |
 -- Module      : GameKeeper.Options
--- Copyright   : (c) 2012 Brendan Hay <brendan@soundcloud.com>
+-- Copyright   : (c) 2012-2015 Brendan Hay <brendan@soundcloud.com>
 -- License     : This Source Code Form is subject to the terms of
 --               the Mozilla Public License, v. 2.0.
 --               A copy of the MPL can be found in the LICENSE file or
